ea363b79bedeb1ba5c2989639c58d0686897b1b3
StephanPagel <stephan.pagel84@gmail.com>
2022-05-19 11:46:42 +0200

    finished

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+//Lvl1_1
+
+// let languages = [
+//     "JavaScript",
+//     "Python",
+//     "Java",
+//     "Ruby",
+//     "PHP",
+//     "C++",
+//     "CSS",
+//     "C#",
+//     "Go",
+//     "C",
+//     "TypeScript",
+//     "Swift"
+// ];
+
+// let sortierung = () =>
+//     console.log(languages.sort());
+
+// sortierung();
+
+//Lvl1_2
+
+// let sortierung2 = () =>
+//     console.log(languages.sort().reverse());
+
+// sortierung2();
+
+//Lvl1_13
+
+// let meinText1 = ["Hello", "World"];
+// let meinText2 = ["Anass", "Elaine", "Eric", "Georg"];
+// let meinText3 = ["Superman", "Wonderwoman", "Hulk", "Batman", "Spiderman"];
+
+// let join1 = meinText1.join();
+// let join2 = meinText1.join("");
+// let join3 = meinText1.join(" ");
+// let join4 = meinText1.join("+");
+// let join5 = meinText2.join();
+// let join6 = meinText2.join("");
+// let join7 = meinText2.join(" ");
+// let join8 = meinText2.join("+");
+// let join9 = meinText3.join();
+// let join10 = meinText3.join("");
+// let join11 = meinText3.join(" ");
+// let join12 = meinText3.join("+");
+
+// console.log(join1);
+// console.log(join2);
+// console.log(join3);
+// console.log(join4);
+// console.log(join5);
+// console.log(join6);
+// console.log(join7);
+// console.log(join8);
+// console.log(join9);
+// console.log(join10);
+// console.log(join11);
+// console.log(join12);
+
+//Lvl2_2
+
+// let numArray2 = [36, 324, 122, 62, 98, 88, 99, 1000];
+
+// numArray2.sort((a, b) => a - b);
+// console.log(numArray2);
